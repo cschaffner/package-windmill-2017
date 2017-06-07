9874d6b97e9139a2b3a9b190bd1a9b4549c8aaed
Christian Schaffner <c.schaffner@uva.nl>
2017-06-08 00:09:08 +0200

    wip

--- hosted.py
+++ hosted.py
@@ -1,6 +1,10 @@
-# Part of info-beamer hosted
 #
-# Copyright (c) 2014, Florian Wesch <fw@dividuum.de>
+# Part of info-beamer hosted. You can find the latest version
+# of this file at:
+# 
+# https://github.com/info-beamer/package-sdk
+#
+# Copyright (c) 2014,2015,2016,2017 Florian Wesch <fw@dividuum.de>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -29,9 +33,10 @@
 
 import os
 import sys
-import json
+import json as json_module
 import socket
 import pyinotify
+from tempfile import NamedTemporaryFile
 
 types = {}
 
@@ -45,6 +50,14 @@ def color(value):
 
 @type
 def string(value):
+    return value
+
+@type
+def text(value):
+    return value
+
+@type
+def section(value):
     return value
 
 @type
@@ -68,8 +81,38 @@ def font(value):
     return value
 
 @type
+def device(value):
+    return value
+
+@type
 def resource(value):
     return value
+
+@type
+def json(value):
+    return value
+
+@type
+def custom(value):
+    return value
+
+def log(msg):
+    print >>sys.stderr, "[hosted.py] %s" % msg
+
+def abort_service(reason):
+    log("restarting service (%s)" % reason)
+    import thread, time
+    try:
+        thread.interrupt_main()
+    except:
+        pass
+    time.sleep(2)
+    os.kill(os.getpid(), 2)
+    time.sleep(2)
+    os.kill(os.getpid(), 15)
+    time.sleep(2)
+    os.kill(os.getpid(), 9)
+    time.sleep(100)
 
 class Configuration(object):
     def __init__(self):
@@ -81,28 +124,24 @@ class Configuration(object):
         self.parse_config_json()
 
     def restart_on_update(self):
-        print >>sys.stderr, "[hosted.py] going to restart when config is updated"
+        log("going to restart when config is updated")
         self._restart = True
 
     def parse_node_json(self, do_update=True):
         with file("node.json") as f:
-            self._options = json.load(f)['options']
+            self._options = json_module.load(f)['options']
         if do_update:
             self.update_config()
 
     def parse_config_json(self, do_update=True):
         with file("config.json") as f:
-            self._config = json.load(f)
+            self._config = json_module.load(f)
         if do_update:
             self.update_config()
 
     def update_config(self):
         if self._restart:
-            print >>sys.stderr, "[hosted.py] restarting service (restart_on_update set)"
-            import thread, time
-            thread.interrupt_main()
-            time.sleep(100)
-            return
+            return abort_service("restart_on_update set")
 
         def parse_recursive(options, config, target):
             # print 'parsing', config
@@ -121,39 +160,32 @@ class Configuration(object):
 
         parsed = {}
         parse_recursive(self._options, self._config, parsed)
-        print >>sys.stderr, "[hosted.py] updated config"
+        log("updated config")
         self._parsed = parsed
 
     def __getitem__(self, key):
         return self._parsed[key]
-Configuration = Configuration()
 
-class EventHandler(pyinotify.ProcessEvent):
-    def process_default(self, event):
-        print >>sys.stderr, event
-        basename = os.path.basename(event.pathname)
-        if basename == 'node.json':
-            Configuration.parse_node_json()
-        elif basename == 'config.json':
-            Configuration.parse_config_json()
-        elif basename == 'hosted.py':
-            print >>sys.stderr, "[hosted.py] restarting service since hosted.py changed"
-            import thread, time
-            thread.interrupt_main()
-            time.sleep(100)
+def setup_inotify(configuration):
+    class EventHandler(pyinotify.ProcessEvent):
+        def process_default(self, event):
+            basename = os.path.basename(event.pathname)
+            if basename == 'node.json':
+                log("node.json changed")
+                configuration.parse_node_json()
+            elif basename == 'config.json':
+                log("config.json changed!")
+                configuration.parse_config_json()
+            elif basename.endswith('.py'):
+                abort_service("python file changed")
 
+    wm = pyinotify.WatchManager()
 
-wm = pyinotify.WatchManager()
+    notifier = pyinotify.ThreadedNotifier(wm, EventHandler())
+    notifier.daemon = True
+    notifier.start()
 
-notifier = pyinotify.ThreadedNotifier(wm, EventHandler())
-notifier.daemon = True
-notifier.start()
-
-wm.add_watch('.', pyinotify.IN_MOVED_TO)
-
-print >>sys.stderr, "initialized hosted.py"
-
-CONFIG = Configuration
+    wm.add_watch('.', pyinotify.IN_MOVED_TO)
 
 class Node(object):
     def __init__(self, node):
@@ -161,7 +193,7 @@ class Node(object):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def send_raw(self, raw):
-        print >>sys.stderr, "sending %r" % (raw,)
+        log("sending %r" % (raw,))
         self._sock.sendto(raw, ('127.0.0.1', 4444))
 
     def send(self, data):
@@ -181,33 +213,93 @@ class Node(object):
 
     def __call__(self, data):
         return self.Sender(self, self._node)(data)
-NODE = Node(os.environ['NODE'])
 
-class Upstream(object):
+    def scratch_cached(self, filename, generator):
+        cached = os.path.join(os.environ['SCRATCH'], filename)
+
+        if not os.path.exists(cached):
+            f = NamedTemporaryFile(prefix='scratch-cached-tmp', dir=os.environ['SCRATCH'])
+            try:
+                generator(f)
+            except:
+                raise
+            else:
+                f.delete = False
+                f.close()
+                os.rename(f.name, cached)
+
+        if os.path.exists(filename):
+            try:
+                os.unlink(filename)
+            except:
+                pass
+        os.symlink(cached, filename)
+
+class Device(object):
     def __init__(self):
         self._socket = None
 
     def ensure_connected(self):
         if self._socket:
-            return False
+            return True
         try:
-            print >>sys.stderr, "establishing upstream connection"
+            log("establishing upstream connection")
             self._socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-            self._socket.connect(os.environ['SYNCER_SOCKET'])
+            self._socket.connect(os.getenv('SYNCER_SOCKET', "/tmp/syncer"))
             return True
         except Exception, err:
-            print >>sys.stderr, "cannot connect to upstream socket: %s" % (err,)
+            log("cannot connect to upstream socket: %s" % (err,))
+            return False
 
     def send_raw(self, raw):
         try:
             if self.ensure_connected():
-                self._socket.send(raw)
+                self._socket.send(raw + '\n')
         except Exception, err:
-            print >>sys.stderr, "cannot send to upstream: %s" % (err,)
+            log("cannot send to upstream: %s" % (err,))
             if self._socket:
                 self._socket.close()
             self._socket = None
 
-    def send(self, **data):
-        self.send_raw(json.dumps(data))
-UPSTREAM = Upstream()
+    def send_upstream(self, **data):
+        self.send_raw(json_module.dumps(data))
+
+    def turn_screen_off(self):
+        self.send_raw("tv off")
+
+    def turn_screen_on(self):
+        self.send_raw("tv on")
+
+    def screen(self, on=True):
+        if on:
+            self.turn_screen_on()
+        else:
+            self.turn_screen_off()
+
+    def reboot(self):
+        self.send_raw("reboot")
+
+    def restart_infobeamer(self):
+        self.send_raw("infobeamer restart")
+
+    def verify_cache(self):
+        self.send_raw("syncer verify_cache")
+
+if __name__ == "__main__":
+    device = Device()
+    while 1:
+        try:
+            command = raw_input("syncer> ")
+            device.send_raw(command)
+        except (KeyboardInterrupt, EOFError):
+            break
+        except:
+            import traceback
+            traceback.print_exc()
+else:
+    log("initialing")
+    NODE = Node(os.environ['NODE'])
+    DEVICE = Device()
+    CONFIG = Configuration()
+    setup_inotify(CONFIG)
+    log("initialized. ready to go!")
