af0c7b2202da0b80289b05ccdd45604a70c93173
Christian Schaffner <c.schaffner@uva.nl>
2017-06-07 21:45:06 +0200

    wip

--- node.lua
+++ node.lua
@@ -411,6 +411,7 @@ local function Scheduler(runner, modules)
 
     local function enqueue(item)
         print('current division is ' .. CONFIG.division)
+        item.options = item.options or {}
         item.options.division = CONFIG.division
         local ok, duration, options = pcall(modules[item.module].prepare, item.options or {})
         if not ok then
