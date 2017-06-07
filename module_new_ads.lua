b50a6ed0503925bcf26194e1dd667b89ad5c293f
Christian Schaffner <c.schaffner@uva.nl>
2017-06-08 00:17:08 +0200

    wip

--- module_new_ads.lua
+++ module_new_ads.lua
@@ -47,8 +47,8 @@ local image_idx = 0
 
 function M.can_schedule()
     print(CONFIG.ads_list)
-    for ad, v in pairs(CONFIG.ads_list) do
-        print('ad item ' .. ad.item .. ' with duration ' .. ad.duration)
+    for i, ad in ipairs(CONFIG.ads_list) do
+        print('ad ' .. i .. ' is item ' .. ad.item .. ' with duration ' .. ad.duration)
     end
 
     return #imglist > 0
